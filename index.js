c4209343f47ad67d647138998bd87a62265aa588
PavelTitov16 <pashoktitov16@gmail.com>
2022-03-04 23:23:58 +0300

    feat:add time

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const startBtn = document.querySelector('#start');
 const screens = document.querySelectorAll('.screen');
 const timeList = document.querySelector('#time-list');
+let time = 0;
+const timer = document.querySelector('#time');
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault;
-    screens[0].classList.add('up')
+    screens[0].classList.add('up');
 })
 
+timeList.addEventListener('click', event => {
+    if (event.target.classList.contains('time-btn')) {
+        time = parseInt(event.target.getAttribute('data-time'));
+        startGame();
+    }
+})
+
+function startGame() {
+    screen[1].classList.add('up');
+    timer.innerHTML = `00:${time}`;
+}
